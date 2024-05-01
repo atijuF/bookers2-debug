8a3e152492c988cc7fb6919641de0e42d13365f8
atijuF <162565598+atijuF@users.noreply.github.com>
2024-05-01 08:17:14 +0000

    [Update]エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    ensure_correct_user
   end
 
   def update
+    ensure_correct_user
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
       render "show"
     end
@@ -30,8 +34,8 @@ class UsersController < ApplicationController
   end
 
   def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
+    user = User.find(params[:id])
+    unless user == current_user
       redirect_to user_path(current_user)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about", as: "about"
+  root to: "homes#top"
+  get "/home/about"=>"homes#about", as: "about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
